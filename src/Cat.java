0e6d7181e9f604a02d9bac3358aedfc38b874ac6
Dmytro Skuratov <bullhead333@gmail.com>
2023-04-25 16:47:16 +0300

    add class Cat

---
+++ src/Cat.java
@@ -0,0 +1,12 @@
+public class Cat {
+
+    private String colour;
+    private boolean sex;
+    private int age;
+
+    public Cat(String colour, boolean sex, int age) {
+        this.colour = colour;
+        this.sex = sex;
+        this.age = age;
+    }
+}
