319b5aeb1e92f2d65aecef444ead39b968aa3673
Dmytro Skuratov <bullhead333@gmail.com>
2023-04-28 13:08:24 +0300

    add getters and setters in class Dog

--- src/Dog.java
+++ src/Dog.java
@@ -9,4 +9,28 @@ public class Dog {
         this.sex = sex;
         this.age = age;
     }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public boolean isSex() {
+        return sex;
+    }
+
+    public void setSex(boolean sex) {
+        this.sex = sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
