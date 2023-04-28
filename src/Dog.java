3c117ed8d01f524de93d1d819bde3d8e7b480ec3
Dmytro Skuratov <bullhead333@gmail.com>
2023-04-28 13:06:58 +0300

    add class Dog

---
+++ src/Dog.java
@@ -0,0 +1,12 @@
+public class Dog {
+
+    private String colour;
+    private boolean sex;
+    private int age;
+
+    public Dog(String colour, boolean sex, int age) {
+        this.colour = colour;
+        this.sex = sex;
+        this.age = age;
+    }
+}
